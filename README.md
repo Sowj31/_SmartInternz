cab575f51b6199872a31f411fb2b35b35f0ecbe7
Sowjanya <83904410+Sowj31@users.noreply.github.com>
2023-08-05 05:32:05 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,32 @@
+# AI Enabled Car Parking System using OpenCV
+The Al-Enabled Car Parking System Utilizing OpenCV Technology is a cutting-edge project
+that aims to revolutionize the way parking lots operate. This system uses OpenCV, a
+popular computer vision library, to enable vehicles to park autonomously.
+
+✔ The OpenCV library analyzes the footage and identifies the available parking
+spaces in the lot.
+
+✔The system is designed to be highly accurate, and it can detect small and large
+vehicles, even in low-light conditions.
+
+# Tech stack :
+python,
+OpenCV,
+HTML,
+CSS,
+Flask
+
+# Screenshots
+![Screenshot (22)](https://github.com/jyoshnz/SmartInternz/assets/116663727/26777f25-e3bf-4a6e-abb0-b32f39372892)
+
+![Screenshot (23)](https://github.com/jyoshnz/SmartInternz/assets/116663727/0b9bd582-2933-4255-8175-69d46fc7fce9)
+
+![Screenshot (83)](https://github.com/jyoshnz/SmartInternz/assets/116663727/7d2bc464-6f7b-4b21-ba12-89116c6c2325)
+
+![Screenshot (85)](https://github.com/jyoshnz/SmartInternz/assets/116663727/27c24efb-c231-4f46-b4dc-deaac57581e3)
+
+![image](https://github.com/jyoshnz/SmartInternz/assets/116663727/959969fc-7c9d-404b-9010-a71864534cce)
+
+
+
+
---
+++ flask/uploads/app.py
@@ -0,0 +1,127 @@
+
+from flask import Flask, render_template, request, session
+import cv2
+import pickle
+import cvzone
+import numpy as np
+import ibm_db
+import re
+
+app = Flask(__name__)
+app.secret_key = 'a'
+conn = ibm_db.connect("DATABASE=bludb; HOSTNAME=6667d8e9-9d4d-4ccb-ba32-21da3bb5aafc.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;PORT=30376;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=hdy10337;PWD=WP4Wy7keo6BGWpL0;", "","")
+print("connected")
+
+@app.route('/')
+def project():
+    return render_template('index.html')
+
+@app.route('/index.html')
+def home():
+    return render_template('index.html')
+
+@app.route('/model')
+def model():
+    return render_template('model.html')
+
+@app.route('/login.html')
+def login():
+    return render_template('login.html')
+
+@app.route('/aboutus.html')
+def aboutus():
+    return render_template('aboutus.html')
+
+@app.route('/signup.html')
+def signup():
+    return render_template('signup.html')
+
+@app.route("/signup", methods=['POST', 'GET'])
+def signup1():
+    msg = ''
+    if request.method == 'POST':
+        name = request.form["name"]
+        email = request.form["email"]
+        password = request.form["password"]
+        insert_sql = "INSERT INTO REGISTER VALUES (?,?,?)"
+        prep_stmt = ibm_db.prepare(conn, insert_sql)
+        ibm_db.bind_param(prep_stmt, 1, name)
+        ibm_db.bind_param(prep_stmt, 2, email)
+        ibm_db.bind_param(prep_stmt, 3, password)
+        ibm_db.execute(prep_stmt)
+        msg = "You have successfully registered!"
+    return render_template('login.html', msg=msg)
+
+@app.route("/login", methods=['POST', 'GET'])
+def login1():
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+        sql = "SELECT * FROM REGISTER WHERE EMAIL=? AND PASSWORD=?"
+        stmt = ibm_db.prepare(conn, sql)
+        ibm_db.bind_param(stmt, 1, email)
+        ibm_db.bind_param(stmt, 2, password)
+        ibm_db.execute(stmt)
+        account = ibm_db.fetch_assoc(stmt)
+        print(account)
+        if account:
+            session['Loggedin'] = True
+            session['id'] = account['EMAIL']
+            session['email'] = account['EMAIL']
+            return render_template('model.html')
+        else:
+            msg = "Incorrect Email/password"
+            return render_template('login.html', msg=msg)
+    else:
+        return render_template('login.html')
+
+
+@app.route('/modelq')
+def liv_pred():
+    # Video feed
+    cap = cv2.VideoCapture('carParkingInput.mp4')
+    with open('parkingSlotPosition', 'rb') as f:
+        posList = pickle.load(f)
+    width, height = 107, 48
+    def checkParkingSpace(imgPro):
+        spaceCounter = 0
+        for pos in posList:
+            x, y = pos
+            imgCrop = imgPro[y:y + height, x:x + width]
+            count = cv2.countNonZero(imgCrop)
+            if count < 900:
+                color = (0, 255, 0)
+                thickness = 5
+                spaceCounter += 1
+            else:
+                color = (0, 0, 255)
+                thickness = 2
+            cv2.rectangle(img, pos, (pos[0] + width, pos[1] + height), color, thickness)
+        cvzone.putTextRect(img, f'Free: {spaceCounter}/{len(posList)}', (100, 50), scale=3, thickness=5, offset=20,
+                       colorR=(0, 200, 0))
+
+
+    while True:
+        if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
+            cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+        success, img = cap.read()
+        if not success:
+            break
+        imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        imgBlur = cv2.GaussianBlur(imgGray, (3, 3), 1)
+        imgThreshold = cv2.adaptiveThreshold(imgBlur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,
+                                             25, 16)
+        imgMedian = cv2.medianBlur(imgThreshold, 5)
+        kernel = np.ones((3, 3), np.uint8)
+        imgDilate = cv2.dilate(imgMedian, kernel, iterations=1)
+        checkParkingSpace(imgDilate)
+        cv2.imshow("Image", img)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    app.run(debug=True)
